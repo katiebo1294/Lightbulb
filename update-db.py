3c9e17c124a230f39af7cd554cbcc19bc2edc022
Katie Borley <borleyk@wwu.edu>
2020-03-03 22:06:33 -0800

    update-db.py now creates a test user: login is test@email.com w/ password 'testing', as well as a class called 'testclass'

--- update-db.py
+++ update-db.py
@@ -1,6 +1,13 @@
-from bettercrative import create_app
+from bettercrative import create_app, bcrypt
+from bettercrative.models import User, Classroom
 app = create_app()
 app.app_context().push()
 from bettercrative import db
 db.drop_all()
 db.create_all()
+hashed_password = bcrypt.generate_password_hash('testing').decode('utf-8')
+user = User(username='testuser', email='test@email.com', password=hashed_password)
+db.session.add(user)
+classroom = Classroom(name='testclass')
+user.classrooms.append(classroom)
+db.session.commit()
