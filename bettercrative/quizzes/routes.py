2e44478efb7cd92f9b17ffd67888163bac5a3369
gabalea <gabalea@wwu.edu>
2020-07-29 12:47:58 -0700

    quiz edit name now works

--- bettercrative/quizzes/routes.py
+++ bettercrative/quizzes/routes.py
@@ -63,18 +63,19 @@ def quiz(quiz_id):
 @quizzes.route("/quiz/edit-name/<int:quiz_id>", methods=['GET', 'POST'])
 @login_required
 def edit_quiz_name(quiz_id):
+    
     quiz = Quiz.query.get_or_404(quiz_id)
     
-    print("editing name")
+    
 
     qzform = QuizForm()
     form = QuestionFormOverall()
-
+    
     if qzform.validate_on_submit():
-        print("Validated")
+        
         quiz.name = qzform.name.data
         db.session.commit()
-        return redirect(url_for('quizzes.quiz', quiz_id=quiz_id))
+        
     return render_template('quiz.html', title=quiz.name, quiz=quiz, qzform=qzform, form=form)
 
 
