c58f75b46f7c1d25670224f118b31ee4e5f8937e
Katie <katieborl@gmail.com>
2020-08-09 13:29:13 -0700

    almost done with quiz.html

--- bettercrative/quizzes/forms.py
+++ bettercrative/quizzes/forms.py
@@ -1,40 +1,44 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, FormField, FieldList, TextAreaField, RadioField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Optional
 from wtforms.widgets import TextArea
 
 class BetterStringField(StringField):
     widget = TextArea()
 
 class AnswerForm(FlaskForm):
-    content = BetterStringField('Answer', validators=[InputRequired('Please fill out this field.')])
+    content = BetterStringField('Answer', validators=[Optional()])
     correct = BooleanField('Correct')
     submit = SubmitField('Save')
 
 
 class QuestionForm(FlaskForm):
-    content = BetterStringField('Question', validators=[InputRequired('Please fill out this field.')])
+    content = BetterStringField('Question', validators=[Optional()])
     submit = SubmitField('Save')
 
 
 class QuizForm(FlaskForm):
-    name = BetterStringField('Name', validators=[InputRequired('Please fill out this field.')])
+    name = BetterStringField('Name', validators=[Optional()])
     submitQuiz = SubmitField('Create Quiz')
 
+
 class TFform(FlaskForm):
-    content = StringField('Answer', validators=[InputRequired('Please fill out this field.')])
+    content = StringField('Answer', validators=[Optional()])
     correct = BooleanField('Correct')
     submit = SubmitField('Save')
-    
+
+
 class QuestionFormOverall(FlaskForm):
     answer_form = FieldList(FormField(AnswerForm))
     question_form = FormField(QuestionForm)
     submit = SubmitField('Save Changes')
 
+
 class QuestionFormOverallTF(FlaskForm):
-    answer_form = FieldList(FormField(TFform))
+    answer_form = FieldList(FormField(TFform), min_entries=2, max_entries=2)
     question_form = FormField(QuestionForm)
     submit = SubmitField('Save Changes')
+
 class QuestionFormOverallSA(FlaskForm):
     answer_form = TextAreaField(u'short answer here')
     question_form = FormField(QuestionForm)
