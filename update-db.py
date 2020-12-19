110ea34292403fa68fce19b830518738d5637309
Katie <borleyk@wwu.edu>
2020-12-19 01:28:15 -0800

    styled datatables on classrooms and quizzes pages

--- update-db.py
+++ update-db.py
@@ -1,3 +1,5 @@
+import random, datetime
+
 from bettercrative import create_app, bcrypt
 from bettercrative.models import User, Classroom, Quiz, Question, Answer
 
@@ -5,22 +7,33 @@ app = create_app()
 app.app_context().push()
 from bettercrative import db
 
+def get_random_date():
+    year = random.randint(2017, 2019)
+    month = random.randint(1, 12)
+    end_day = 30
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        end_day += 1
+    elif month == 2:
+        end_day -= 2
+    day = random.randint(1, end_day)
+    return datetime.date(year, month, day)
+
 db.drop_all()
 db.create_all()
 # Katie's test account
 katie_password = bcrypt.generate_password_hash('testing').decode('utf-8')
 katie = User(username='testuser', email='test@email.com', password=katie_password)
 db.session.add(katie)
-katie_classroom1 = Classroom(name='Computing Education (CSCI 497R)')
-katie_classroom2 = Classroom(name='Computer Systems II (CSCI 347)')
-katie_classroom3 = Classroom(name='Calculus II (MATH 225)')
-katie_classroom4 = Classroom(name='Computer Programming & Linear Data Structures (CSCI 145)')
-katie_classroom5 = Classroom(name='Probability & Statistics (MATH 341)')
-katie.classrooms.append(katie_classroom1)
-katie.classrooms.append(katie_classroom2)
-katie.classrooms.append(katie_classroom3)
-katie.classrooms.append(katie_classroom4)
-katie.classrooms.append(katie_classroom5)
+names = ['Computing Education (CSCI 497R)', 'Computer Systems II (CSCI 347)', 'Calculus II (MATH 225)', 'Computer Programming & Linear Data Structures (CSCI 145)', 'Probability & Statistics (MATH 341)']
+for i in range(0, 4):
+    katie_classroom = Classroom(name=names[i], date_created=get_random_date())
+    while True:
+        date_modified = get_random_date()
+        if date_modified >= katie_classroom.date_created:
+            break
+    katie_classroom.date_modified = date_modified
+    katie.classrooms.append(katie_classroom)
+
 # two quizzes, each with 2 MC questions and 4 answers to each
 quiz_1 = Quiz(name='Computer Science Quiz')
 question_1 = Question(name='Question 1', content='Who is the best CS professor?', category='Multiple Choice')
@@ -66,15 +79,26 @@ question_2.answers.append(answer_2_2)
 question_2.answers.append(answer_2_3)
 question_2.answers.append(answer_2_4)
 katie.quizzes.append(quiz_2)
+
+
+# add some empty quizzes to test the quiz datatable
+for i in range(1, 17):
+    date_created = get_random_date()
+    while True:
+        date_modified = get_random_date()
+        if date_modified >= date_created:
+            break
+    quiz = Quiz(name='Quiz ' + str(i), date_created=date_created, date_modified=date_modified)
+    katie.quizzes.append(quiz)
 # add CS quiz to the CS classrooms and make it active in CSCI 497R
-katie_classroom1.added_quizzes.append(quiz_1)
-katie_classroom2.added_quizzes.append(quiz_1)
-katie_classroom3.added_quizzes.append(quiz_1)
-katie_classroom1.active_quiz = quiz_1.id
+katie.classrooms[1].added_quizzes.append(quiz_1)
+katie.classrooms[2].added_quizzes.append(quiz_1)
+katie.classrooms[3].added_quizzes.append(quiz_1)
+katie.classrooms[1].active_quiz = quiz_1.id
 # add math quiz to the math classrooms and make it active in MATH 225
-katie_classroom3.added_quizzes.append(quiz_2)
-katie_classroom5.added_quizzes.append(quiz_2)
-katie_classroom3.active_quiz = quiz_2.id
+katie.classrooms[3].added_quizzes.append(quiz_2)
+katie.classrooms[5].added_quizzes.append(quiz_2)
+katie.classrooms[3].active_quiz = quiz_2.id
 db.session.commit()
 # Tim's test account
 tim_password = bcrypt.generate_password_hash('test').decode('utf-8')
